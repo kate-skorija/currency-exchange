4b6473fba13c9ee4d4aac970ea707b4b1f2378da
Kate Skorija <kateskorija@gmail.com>
2020-07-03 07:05:19 -0700

    refactor UI logic, create separate convertCurrency function

--- src/exchange-service.js
+++ src/exchange-service.js
@@ -1,12 +1,26 @@
 export async function getRates() {
   try {
-    let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latst/USD`);
+    let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
     let jsonifiedResponse = await response.json();
     return jsonifiedResponse;
   } catch(error) {
     return error;
   }
 }
+
+// try {
+//   let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latst/USD`);
+//   let jsonifiedResponse;
+  
+//   if (response.ok && response.status == 200) {
+//     jsonifiedResponse = await response.json();
+//   } else {
+//     jsonifiedResponse = false;
+//   }
+//   return jsonifiedResponse;
+// } catch(error) {
+//   return error;
+// }
 
 // try {
 //   let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latst/USD`);
--- src/main.js
+++ src/main.js
@@ -10,29 +10,35 @@ $(document).ready(function() {
     const usd = parseInt($("#usdInput").val());
     const otherCurrency = $("#otherCurrency").val();
 
-    (async () => {
+    async function displayResults() {
       const apiResponse = await getRates();
-      console.log(apiResponse);
       if (apiResponse instanceof Error) {
         $("#results").html(`There has been an error processing your request: ${apiResponse}.`);
-      } else if (otherCurrency === "AED") {
-        let convertedAmount = (usd * (apiResponse.conversion_rates.AED).toFixed(2));
+      } else {
+        convertCurrency(apiResponse);
+      }
+    }
+
+    function convertCurrency(responseParam){
+      if (otherCurrency === "AED") {
+        let convertedAmount = (usd * (responseParam.conversion_rates.AED).toFixed(2));
         $("#results").text(`${convertedAmount} AED`);
       } else if (otherCurrency === "ARS") {
-        let convertedAmount = (usd * (apiResponse.conversion_rates.ARS).toFixed(2));
+        let convertedAmount = (usd * (responseParam.conversion_rates.ARS).toFixed(2));
         $("#results").text(`${convertedAmount} ARS`);
       } else if (otherCurrency === "AUD") {
-        let convertedAmount = (usd * (apiResponse.conversion_rates.AUD).toFixed(2));
+        let convertedAmount = (usd * (responseParam.conversion_rates.AUD).toFixed(2));
         $("#results").text(`${convertedAmount} AUD`);
       } else if (otherCurrency === "BGN") {
-        let convertedAmount = (usd * (apiResponse.conversion_rates.BGN).toFixed(2));
+        let convertedAmount = (usd * (responseParam.conversion_rates.BGN).toFixed(2));
         $("#results").text(`${convertedAmount} BGN`);
       } else if (otherCurrency === "BRL") {
-        let convertedAmount = (usd * (apiResponse.conversion_rates.BRL).toFixed(2));
+        let convertedAmount = (usd * (responseParam.conversion_rates.BRL).toFixed(2));
         $("#results").text(`${convertedAmount} BRL`);
-      } else if (otherCurrency !== "AED" || otherCurrency !== "ARS" || otherCurrency !== "AUD" || otherCurrency !== "BGN" || otherCurrency !== "BRL") {
+      } else {
         $("#results").text("This currency exchanger does not support that particular currency. Please select another.");
       } 
-    })();
+    }
+    displayResults();
   });
 });
