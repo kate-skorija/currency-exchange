508814349ed8fb1a775fc840e5941a820cc2e3b4
Kate Skorija <kateskorija@gmail.com>
2020-07-03 06:32:20 -0700

    refactor business logic, eliminate if/else statement since only jsonifiedResponse is needed

--- src/exchange-service.js
+++ src/exchange-service.js
@@ -1,27 +1,23 @@
 export async function getRates() {
   try {
     let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latst/USD`);
-    let jsonifiedResponse;
-    if (!response.ok) {
-      jsonifiedResponse = response.statusText;
-      throw Error("Error: " + jsonifiedResponse);
-    } else {
-      jsonifiedResponse = await response.json();
-    }
+    let jsonifiedResponse = await response.json();
     return jsonifiedResponse;
   } catch(error) {
-    console.error(`Error: ${error.message}`);
     return error;
   }
 }
 
-// export async function getRates() {
-//   let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+// try {
+//   let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latst/USD`);
 //   let jsonifiedResponse;
-//   if (response.ok && response.status == 200) {
+  
+//   if (!response.ok) {
 //     jsonifiedResponse = await response.json();
 //   } else {
-//     jsonifiedResponse = response.statusText;
+//     jsonifiedResponse = await response.json();
 //   }
 //   return jsonifiedResponse;
+// } catch(error) {
+//   return error;
 // }
