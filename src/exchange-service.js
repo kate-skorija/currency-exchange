0d95a5e86a8ba270b40377f3853d5032ada18599
Kate Skorija <kateskorija@gmail.com>
2020-07-02 18:46:47 -0700

    add branching to UI API logic to display converted amounts of the 5 different currencies

--- src/exchange-service.js
+++ src/exchange-service.js
@@ -1,6 +1,6 @@
 export async function getRates() {
   try {
-    let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+    let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
     let jsonifiedResponse;
     if (response.ok && response.status == 200) {
       jsonifiedResponse = await response.json();
--- src/main.js
+++ src/main.js
@@ -7,16 +7,32 @@ import './styles.css';
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    // const usd = parseInt($("#usdInput").val());
-    // const otherCurrency = $("#otherCurrency").val();
+    const usd = parseInt($("#usdInput").val());
+    const otherCurrency = $("#otherCurrency").val();
 
     (async () => {
       const apiResponse = await getRates();
-      console.log(apiResponse);
       if(!apiResponse) {
-        $(".errors").html(`There has been an error processing your request.`)
+        $(".errors").html(`There has been an error processing your request.`);
       } else {
-        $("#results").text(apiResponse.conversion_rates.AED);
+        if (otherCurrency === "AED") {
+          let convertedAmount = usd * (apiResponse.conversion_rates.AED);
+          $("#results").text(convertedAmount);
+        } else if (otherCurrency === "ARS") {
+          let convertedAmount = usd * (apiResponse.conversion_rates.ARS);
+          $("#results").text(convertedAmount);
+        } else if (otherCurrency === "AUD") {
+          let convertedAmount = usd * (apiResponse.conversion_rates.AUD);
+          $("#results").text(convertedAmount);
+        } else if (otherCurrency === "BGN") {
+          let convertedAmount = usd * (apiResponse.conversion_rates.BGN);
+          $("#results").text(convertedAmount);
+        } else if (otherCurrency === "BRL") {
+          let convertedAmount = usd * (apiResponse.conversion_rates.BRL);
+          $("#results").text(convertedAmount);
+        } else {
+          $("#results").text("This currency exchanger does not support that particular currency. Please select another.");
+        }
       }
     })();
   });
