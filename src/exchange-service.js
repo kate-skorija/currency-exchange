6829c1a90ed06531ba2cd2e5fd895c9b6c243a08
Kate Skorija <kateskorija@gmail.com>
2020-07-02 17:58:00 -0700

    create business logic for API call

---
+++ src/exchange-service.js
@@ -0,0 +1,14 @@
+export async function getRates() {
+  try {
+    let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+    let jsonifiedResponse;
+    if (response.ok && response.status == 200) {
+      jsonifiedResponse = await response.json();
+    } else {
+      jsonifiedResponse = false;
+    }
+    return jsonifiedResponse;
+  } catch(error) {
+    return false;
+  }
+}
