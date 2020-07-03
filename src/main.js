c08baa03fe21a3c6f5d2a2a4d03ea69e990a7ebf
Kate Skorija <kateskorija@gmail.com>
2020-07-02 18:29:55 -0700

    add UI API logic to display a currency rate to the DOM

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
-// import { x } from './x.js';
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './styles.css';
+import { getRates } from './exchange-service.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    // const usd = parseInt($("#usdInput").val());
+    // const otherCurrency = $("#otherCurrency").val();
+
+    (async () => {
+      const apiResponse = await getRates();
+      console.log(apiResponse);
+      if(!apiResponse) {
+        $(".errors").html(`There has been an error processing your request.`)
+      } else {
+        $("#results").text(apiResponse.conversion_rates.AED);
+      }
+    })();
+  });
+});
